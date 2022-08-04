740bf9fd989904f269c84318ed58455ac0466909
Ashraful Gafur <ashraful.gafur@streamstech.com>
2022-08-04 15:36:32 +0600

    search for artists and tracs have been added

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const SpotifyWebApi = require('spotify-web-api-node');
+const accessToken = '';
+const refreshToken = '';
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
@@ -13,7 +15,7 @@ admin.initializeApp({
 const db = admin.firestore();
 const User = db.collection("users");
 
-/* SPOTIFY WEB API NODE
+/* SPOTIFY WEB API NODE */
 const scopes = [
    'ugc-image-upload',
    'user-read-playback-state',
@@ -39,9 +41,49 @@ const scopes = [
 const spotifyApi = new SpotifyWebApi({
    redirectUri: 'http://localhost:8080/callback',
    clientId: 'ec3a3e42f6e2401e8d86c66603cd5642',
-   clientSecret: process.argv.slice(2)[1]
+   clientSecret: '5dde3e44b3ff4440b875f6460a4fbdf4'
 });
-*/
+app.get('/login', (req,res)=>{
+   res.redirect(spotifyApi.createAuthorizeURL(scopes));
+})
+app.get('/callback', function(req, res) {
+   let code  = req.query['code'];
+   let state  = req.query['state'];
+   let error  = req.query['error'];
+   if(error){
+      console.log("Callback Error:",error);
+      res.send(error)
+      return ;
+   }
+   spotifyApi.authorizationCodeGrant(code)
+       .then((data) =>{
+          // Set the access token on the API object to use it in later calls
+          const accessToken = data.body['access_token'];
+          const refreshToken = data.body['refresh_token'];
+          spotifyApi.setAccessToken(accessToken);
+          spotifyApi.setRefreshToken(refreshToken);
+          console.log('access token = ',accessToken);
+          console.log('Refresh token = ',refreshToken);
+          res.send("Successfully retrieve Access Token");
+         /* spotifyApi.searchTracks("Love").then(result=>{
+             console.log(result.body.tracks.items);
+          });*/
+       }, function(err) {
+          console.log('Something went wrong!', err);
+       });
+});
+app.get('/search',(req,res)=>{
+   const requestParams = req.body.searchKey;
+   console.log(requestParams)
+   spotifyApi.searchTracks(requestParams).then(result=>{
+      res.json(result);
+   });
+});
+app.get('/getArtists', async (req,res)=>{
+   spotifyApi.getMyTopArtists().then(result=>{
+      res.json(result);
+   });
+});
 
 app.post('/signup', async (req,res)=>{
    console.log(req.body);
